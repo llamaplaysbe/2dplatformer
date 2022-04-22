a9550e39b70ade2b507214764965c1870ee28a34
Ben Davies-Moore <bdmoore@pop-os.localdomain>
2022-04-22 17:51:13 +0100

    Added shake when in the air

--- Scenes/GameCamera.gd
+++ Scenes/GameCamera.gd
@@ -3,15 +3,41 @@ extends Camera2D
 var targetPosition = Vector2.ZERO
 
 export(Color, RGB) var backroundColor
+export(OpenSimplexNoise) var shakeNoise
+
+var xNoiseSampleVector = Vector2.RIGHT
+var yNoiseSampleVector = Vector2.DOWN
+var xNoiseSamplePostion = Vector2.ZERO
+var yNoiseSamplePostion = Vector2.ZERO
+var noiseSampleTravelRate = 300
+var maxShakeOffset = 5
+var currentShakePercentage = 0
+var shakeDecay = 2
 
 func _ready():
 	VisualServer.set_default_clear_color(backroundColor)
-
+	
 func _process(delta):
 	acquire_target_position()
 	
 	global_position = lerp(targetPosition, global_position, pow(2, -15 * delta))
+	
+	if (Input.is_action_just_pressed("jump")):
+		apply_shake(1)
+	
+	if(currentShakePercentage > 0):
+		xNoiseSamplePostion += xNoiseSampleVector * noiseSampleTravelRate * delta
+		yNoiseSamplePostion += yNoiseSampleVector * noiseSampleTravelRate * delta
+		var xSample = shakeNoise.get_noise_2d(xNoiseSamplePostion.x, xNoiseSamplePostion.y)
+		var ySample = shakeNoise.get_noise_2d(yNoiseSamplePostion.x, yNoiseSamplePostion.y)
+		
+		var calculatedOffset = Vector2(xSample, ySample) * maxShakeOffset * currentShakePercentage
+		offset = calculatedOffset
+		
+		currentShakePercentage = clamp(currentShakePercentage - shakeDecay * delta, 0, 1)
 
+func apply_shake(percentage):
+	currentShakePercentage = clamp(currentShakePercentage + percentage, 0, 1)
 
 func acquire_target_position():
 	var players = get_tree().get_nodes_in_group("player")
